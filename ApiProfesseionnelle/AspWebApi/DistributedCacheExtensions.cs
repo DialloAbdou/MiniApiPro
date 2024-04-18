4ff469a3911304ef53c9efec5c7c04864156ef1a
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-04-18 21:46:58 +0200

    CacheMemoire Distribuée Avec Redis

---
+++ ApiProfesseionnelle/AspWebApi/DistributedCacheExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace AspWebApi
+{
+    public static class DistributedCacheExtensions
+    {
+        public static async Task<T?> GetAsync<T>(this IDistributedCache cache, string key) where T : class
+        {
+            var json = await cache.GetStringAsync(key);
+            if (string.IsNullOrEmpty(json)) return default;
+            return JsonSerializer.Deserialize<T>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            });
+        }
+
+        public static async Task SetAsync<T>(this IDistributedCache cache, string key, T value) where T: class 
+        {
+            var json = JsonSerializer.Serialize(value);
+            await cache.SetStringAsync(key, json);
+        }
+    }
+}
--- ApiProfesseionnelle/AspWebApi/Program.cs
+++ ApiProfesseionnelle/AspWebApi/Program.cs
@@ -9,8 +9,10 @@ using Microsoft.EntityFrameworkCore;
 using System.Threading;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Distributed;
+using AspWebApi;
 
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(args);
 
 //------------Configuration----------------
 
@@ -30,19 +32,26 @@ builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 //----------Configuration de la cache mémoire ------------
 //builder.Services.AddMemoryCache();
-builder.Services.AddOutputCache(op =>
+
+//builder.Services.AddOutputCache(op =>
+//{
+//    //ajoute moi la prise en charge du cache de la sotie voici la strategie dont j'ai choisie 
+
+//    op.AddBasePolicy(b => b.Expire(TimeSpan.FromMinutes(1)));
+//    op.AddPolicy("Expire2mn", b => b.Expire(TimeSpan.FromMinutes(2)));
+//    op.AddPolicy("Expire10secds", b => b.Expire(TimeSpan.FromSeconds(10)).Tag("personnes"));
+//    op.AddPolicy("ById", b => b.SetVaryByRouteValue("ById"));
+//});
+
+
+// utlisation de cache partagé ou distribuée
+//builder.Services.AddDistributedMemoryCache();
+builder.Services.AddStackExchangeRedisCache(opt =>
 {
-    //ajoute moi la prise en charge du cache de la sotie voici la strategie dont j'ai choisie 
-
-    op.AddBasePolicy(b => b.Expire(TimeSpan.FromMinutes(1)));
-    op.AddPolicy("Expire2mn", b => b.Expire(TimeSpan.FromMinutes(2)));
-    op.AddPolicy("Expire10secds", b => b.Expire(TimeSpan.FromSeconds(10)));
-    op.AddPolicy("ById", b => b.SetVaryByRouteValue("ById"));
-
-
+    opt.Configuration = "localhost:6379";
 });
 var app = builder.Build();
-app.UseOutputCache();
+//app.UseOutputCache();
 //--------Création Base de donnée -----------------------
 app.Services
     .CreateScope().ServiceProvider.
@@ -57,41 +66,42 @@ app.MapGet("/personnes", async ([FromServices] PersonneDbContext context) =>
 
 app.MapGet("/personnes/{id:int}", async (
     [FromRoute] int id,
-    [FromServices] PersonneDbContext context
-    /*[FromServices] IMemoryCache cache*/) =>
+    [FromServices] PersonneDbContext context,
+     /*[FromServices] IMemoryCache cache*/
+     [FromServices] IDistributedCache cache) =>
    {
-       #region CacheMemoire
-       //if (!/*cache.TryGetValue<Personne>($"personne_{id}", out var person*/))
-       //{
-       //    person = await context.Personnes.FirstOrDefaultAsync(p => p.Id == id);
-       //    if (person is null) return Results.NotFound("cette personne n'existe");
-       //    cache.Set($"personne_{id}", person);
-       //    return Results.Ok(person);
-       //}
+       #region CacheMemoireDistribuee
+       var person = await cache.GetAsync<Personne>($"personne_{id}");
+       if (person is null)
+       {
+           person = await context.Personnes.FirstOrDefaultAsync(p => p.Id == id); // on la recupere depuis la base de donnée
+           if (person is null) return Results.NotFound("cette personne n'existe");
+           await cache.SetAsync($"personne_{id}", person);
+           return Results.Ok(person);
+       }
        #endregion
-       var person = await context.Personnes.SingleOrDefaultAsync(p => p.Id == id);
-       if (person is null) return Results.NotFound("cette personne n'existe pas !");
        return Results.Ok(person);
-   }).CacheOutput("Expire10secds");
+   }).CacheOutput();
 
 
 //------------------Update-----------------------------------------
 app.MapPut("/personnes/{id:int}", async (
     [FromRoute] int id,
     [FromServices] PersonneDbContext context,
-    [FromBody] Personne personne
-   /* [FromServices] IMemoryCache cache*/) =>
+    [FromBody] Personne personne,
+      /* [FromServices] IMemoryCache cache*/
+      [FromServices] IDistributedCache cache) =>
 {
     //-----------Version EF7-----------
     var result = await context.Personnes.Where(p => p.Id == id)
     .ExecuteUpdateAsync(pe => pe
     .SetProperty(pe => pe.Nom, personne.Nom)
     .SetProperty(pe => pe.Prenom, personne.Prenom));
-    //if (result > 0)
-    //{
-    //    cache.Remove($"personne_{id}");
-    //    return Results.NoContent();
-    //}
+    if (result > 0)
+    {
+        await cache.RemoveAsync($"personne_{id}");
+        return Results.NoContent();
+    }
     if (result > 0) return Results.Content("Validé");
     return Results.NotFound();
 
